e5834d1e7d354b5d796f69d3bc7593f4d7e7be72
Vivexa Tech <vivexatech@gmail.com>
2025-08-22 16:35:44 +0530

    Update create-config.sh

--- create-config.sh
+++ create-config.sh
@@ -5,7 +5,7 @@ echo "const config = {
     apiKey: '$VITE_FIREBASE_API_KEY',
     authDomain: '$VITE_FIREBASE_AUTH_DOMAIN',
     projectId: '$VITE_FIREBASE_PROJECT_ID',
-    storageBucket: '$VITE_FIREBASE_STORAGE_BUCKET',
+    storageBucket: '$VITE_FIREBASE_STORAGE_BUCKET', 
     messagingSenderId: '$VITE_FIREBASE_SENDER_ID',
     appId: '$VITE_FIREBASE_APP_ID'
   }, 
