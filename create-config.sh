b8be6a2db15c58b7184b16dafb086ad3db0bdfbb
Vivexa Tech <vivexatech@gmail.com>
2025-08-22 16:18:44 +0530

    Create create-config.sh

---
+++ create-config.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script creates the config.js file using Vercel's Environment Variables.
+
+echo "const config = {
+  firebase: {
+    apiKey: '$VITE_FIREBASE_API_KEY',
+    authDomain: '$VITE_FIREBASE_AUTH_DOMAIN',
+    projectId: '$VITE_FIREBASE_PROJECT_ID',
+    storageBucket: '$VITE_FIREBASE_STORAGE_BUCKET',
+    messagingSenderId: '$VITE_FIREBASE_SENDER_ID',
+    appId: '$VITE_FIREBASE_APP_ID'
+  },
+  cloudinary: {
+    cloudName: '$VITE_CLOUDINARY_CLOUD_NAME',
+    uploadPreset: '$VITE_CLOUDINARY_UPLOAD_PRESET'
+  },
+  razorpay: {
+    keyId: '$VITE_RAZORPAY_KEY_ID'
+  }
+};" > config.js
