7a8358b369e9d1e51520ef9e99da00039904cc0e
Vivexa Tech <vivexatech@gmail.com>
2025-08-22 17:04:30 +0530

    Update create-config.js

--- create-config.js
+++ create-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 var firebaseConfig = {
-  apiKey: "AIzaSyArx3eeAj98XkXJqMtITF2I00sjm-LQDqs",
-  authDomain: "vivexa-admin.firebaseapp.com",
-  projectId: "vivexa-admin",
-  storageBucket: "vivexa-admin.firebasestorage.app",
-  messagingSenderId: "68717817629",
-  appId: "1:68717817629:web:a2a6dc32979f2b2d1fbbf4",
-};
+      apiKey: "AIzaSyCFUnalA7gBmwYqCXfsRtZugBfDWpT_uQo",
+      authDomain: "demoshop-cb486.firebaseapp.com",
+      projectId: "demoshop-cb486",
+      storageBucket: "demoshop-cb486.firebasestorage.app",
+      messagingSenderId: "773221651282",
+      appId: "1:773221651282:web:1ca1e2527ecd584675e08b"
+    };
